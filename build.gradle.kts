5a4bff4c5236c421cfb2d263065100a43e259716
Eric Haag <ehaag@gradle.com>
2023-05-16 11:31:59 -0500

    Add asciidoctor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
-    id("base")
+    id("org.asciidoctor.jvm.pdf") version "3.3.2"
+    id("org.asciidoctor.jvm.gems") version "3.3.2"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
