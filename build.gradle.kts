1777ae7b2344b9952b536f7f916aa55481902dc9
Eric Haag <ehaag@gradle.com>
2023-05-16 11:38:41 -0500

    Configure asciidoctor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
+import com.gradle.scan.plugin.BuildScanExtension
+
 plugins {
-    id("org.asciidoctor.jvm.pdf") version "3.3.2"
     id("org.asciidoctor.jvm.gems") version "3.3.2"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val buildScanApi = project.extensions.findByName("buildScan") as BuildScanExtension
+
+tasks {
+    build {
+        dependsOn(asciidoctor)
+    }
+
+    asciidoctor {
+        doFirst {
+            buildScanApi.value("$identityPath#gemPath", inputs.properties["gemPath"].toString())
+        }
+        sources("index.adoc")
+        sourceDirProperty.set(layout.projectDirectory.dir("docs"))
+        outputDirProperty.set(layout.buildDirectory.dir("docs"))
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,28 @@
-rootProject.name = "gempath-cache-miss-reproducer"
+plugins {
+    id("com.gradle.enterprise") version "3.13.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.10"
+}
+
+val isCI = !System.getenv("CI").isNullOrEmpty()
+
+gradleEnterprise {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        capture { isTaskInputFiles = true }
+        isUploadInBackground = !isCI
+        publishAlways()
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(gradleEnterprise.buildCache) {
+        isEnabled = true
+        isPush = isCI
+    }
+}
+
+rootProject.name = "asciidoctor-cache-miss-reproducer"
